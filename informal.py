732ccd9da09477d7f0f32630223d323111a0d575
Margaret <margaret.ach7@gmail.com>
2023-12-20 16:52:34 -0500

    basic stuff

---
+++ informal.py
@@ -0,0 +1,25 @@
+message = "Hello, World"
+print(message)
+
+#Numbers
+1 + 1
+2 - 1
+5 * 3
+10 / 2
+12 // 2
+2 ** 2
+
+#Text
+
+'I like cake'
+"Cheesecake is good"
+"2023"
+
+quote = "She said, \"I am doing okay.\""
+print(quote)
+
+newline = "Goodbye\nHello"
+print(newline)
+
+rawString = r'C:\user\name'
+print(rawString)
